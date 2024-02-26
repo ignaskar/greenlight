dbb80e027bf56c5cfd502a5fe91e2f4add4612ba
ignas <ignas.karpusenkovas@gmail.com>
2024-02-26 14:56:04 +0200

    optimistic locking and partial update support

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -59,3 +59,9 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+// editConflictResponse is used to send a 409 Conflict status code and JSON response to the client when editing a movie
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
